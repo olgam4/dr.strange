1dba420fede06b368f6216063b8cdf4477191ab5
Olivier Gamache <olivier@glo.quebec>
2022-05-13 15:31:25 -0400

    ✨ Enhance timer logic with stop and reset

--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -9,29 +9,57 @@ interface Props {
 
 export default ({ minutes }: Props) => {
   const [timer, setTimer] = createSignal(minutes * 60)
-  const [started, setStared] = createSignal(false)
+  const [started, setStarted] = createSignal(false)
+  const [timerInterval, setTimerInterval] = createSignal(0)
 
   const startTimer = () => {
     if (started()) return
-    setStared(true)
-    setInterval(() => {
+    setStarted(true)
+    const int = setInterval(() => {
       setTimer(t => t - 1)
       if (timer() === 0) {
         const audio = new Audio(Ding)
         audio.play()
+        stopTimer()
       }
     }, 1000)
+    setTimerInterval(int)
+  }
+
+  const stopTimer = () => {
+    setStarted(false)
+    clearInterval(timerInterval())
+  }
+   const resetTimer = () => {
+    setTimer(minutes * 60)
+    stopTimer()
   }
 
   return (
     <div class="flex flex-col justify-center items-center rounded-md px-2 py-4 mx-60 bg-neutral-400">
       <div>{fmtMSS(timer())}</div>
-      <button
-        onClick={startTimer}
-        class="border rounded-md px-2 py-1 border-b-4"
-      >
-        Start
-      </button>
+      <div class="flex justify-center">
+        <button
+          onClick={startTimer}
+          class="border rounded-md px-2 py-1 border-b-4"
+          disabled={started()}
+        >
+          Start
+        </button>
+        <button
+          onClick={stopTimer}
+          class="border rounded-md px-2 py-1 border-b-4"
+          disabled={!started()}
+        >
+          Stop
+        </button>
+        <button
+          onClick={resetTimer}
+          class="border rounded-md px-2 py-1 border-b-4"
+        >
+          Reset
+        </button>
+      </div>
     </div>
   )
 }
