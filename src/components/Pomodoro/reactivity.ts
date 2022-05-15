5f8c0d982fda49bbb5b8d0b311286617256c5d3c
Olivier Gamache <olivier@glo.quebec>
2022-05-14 20:55:19 -0400

    🤘 Fix how the Pomodoro State redirects you

--- src/components/Pomodoro/reactivity.ts
+++ src/components/Pomodoro/reactivity.ts
@@ -37,7 +37,7 @@ const createPomodoro = () => {
         case State.POMODORO:
           return setShortBreak()
         case State.SHORT_BREAK:
-          return setLongBreak()
+          return setPomodoro()
         case State.LONG_BREAK:
           return setPomodoro()
       }
